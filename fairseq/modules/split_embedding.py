7094e8b13669e90ef937cbf6ae8cbb4576cfa6a3
Nichole Hansen <nicholehansen@pop-os.localdomain>
2021-12-29 12:10:31 -0800

    removed some comments and breakpoints, cleaned up assertions inside of SplitEmbedding forward function.

--- fairseq/modules/split_embedding.py
+++ fairseq/modules/split_embedding.py
@@ -1,16 +1,18 @@
-import torch.nn as nn
+import pdb
 
+import torch.nn as nn
+import pdb
 class SplitEmbedding(nn.Module):
     def __init__(self, embeddings_one, embeddings_two):
         super().__init__()
-        #TODO: fix pad index
         self.embeddings_one = embeddings_one
         self.embeddings_two = embeddings_two
         self.embeddings_one_vocab_size = self.embeddings_one.num_embeddings
 
     def forward(self, indices):
         #only batch size of 1 is supported for now.
-        assert indices.dim() == 1
+        assert indices.shape[0] == 1
+        indices.squeeze()
         t_result = indices - (self.embeddings_one_vocab_size - 1)
         t_result[t_result < 0] = 0
         emb_two = self.embeddings_two(t_result)
--- scripts/test_split_embeddings.py
+++ scripts/test_split_embeddings.py
@@ -3,7 +3,7 @@ from fairseq.data.encoders import subword_nmt_bpe as bpe
 import pdb
 from fairseq.data.dictionary import *
 import torch.nn as nn
-from fairseq.fairseq.modules.split_embedding import *
+from fairseq.modules.split_embedding import *
 
 
 def Embedding(num_embeddings, embedding_dim, other_idx=0):
@@ -19,27 +19,22 @@ def build_embedding( dictionary, embed_dim):
     return emb
 
 def main():
-    en2de2 = torch.hub.load('nikijean/fairseq:main', 'transformer.wmt16.en-de', tokenizer='moses', bpe='subword_nmt')
-    initial_dict_length = len(en2de2.src_dict)
-    new_bpe = bpe.SubwordNMTBPE(en2de2.cfg['bpe'], glossaries=["snorflblab", "barflbla"])
-    en2de2.bpe = new_bpe
-    #pdb.set_trace()
-    print(en2de2.bpe.encode("I am a snorflblab barflbla"))
-    print(en2de2.encode("I am a snorflblab barflbla", True))
-
-    cfg = en2de2.models[0].cfg
-    #sub_d = SubDictionary(en2de2.src_dict, 32768)
-
-    num_embeddings = len(en2de2.src_dict) - initial_dict_length + 1 #plus one for the 0 embedding
-    new_embeddings = Embedding(num_embeddings, 1024)
-        #en2de2.models[0].build_embedding(cfg, sub_d, 1024)
-
-    t = torch.LongTensor([0, 1])
-    t2 = en2de2.encode("I am a snorflblab barflbla")
-
-    split_embeddings = SplitEmbedding(en2de2.models[0].encoder.embed_tokens, new_embeddings)
-    emb = split_embeddings(t2)
+    en2de = torch.hub.load('nikijean/fairseq:main', 'transformer.wmt16.en-de', tokenizer='moses', bpe='subword_nmt')
+    initial_dict_length = len(en2de.src_dict)
+    new_bpe = bpe.SubwordNMTBPE(en2de.cfg['bpe'], glossaries=["snorflblab", "barflbla"])
+    en2de.bpe = new_bpe
     pdb.set_trace()
+    #TODO: we actually need to update the src_dict from file, rather than doing it this way.
+    #setting the second argument to True forces the word to be addesd to the dictionary.
+    encoded_result = en2de.encode("I am a snorflblab barflbla", True) #True adds new word to src_dict if it's not there
+
+
+    num_embeddings = len(en2de.src_dict) - initial_dict_length + 1 #plus one for the 0 embedding
+    new_embeddings = Embedding(num_embeddings, 1024)
+
+    split_embeddings = SplitEmbedding(en2de.models[0].encoder.embed_tokens, new_embeddings)
+    en2de.models[0].encoder.embed_tokens = split_embeddings
+    result = en2de.translate("Hello world")
 
 if __name__ == "__main__":
     main()
